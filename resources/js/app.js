6f98fc820c562ccbf9baa0dcdcd98e973fe0a7db
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2019-12-10 19:04:51 +0330

    create component for CM broadcasting

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@
 
 require('./bootstrap');
 
-// window.Vue = require('vue');
+window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +19,7 @@ require('./bootstrap');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('Comment', require('./components/CommentComponent.vue').default);
 // Vue.component(
 //     'passport-clients',
 //     require('./components/passport/Clients.vue').default,
@@ -41,6 +41,6 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-// const app = new Vue({
-//     el: '#app',
-// });
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/CommentComponent.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="container">
+
+        <span class="label label-success" style="margin-left:15px;" v-if="post.published">Published</span>
+
+        <span class="label label-default" style="margin-left:15px;" v-else>Draft</span>
+
+        <hr/>
+        <p class="lead">
+            {{post.content}}
+        </p>
+        <hr/>
+
+        <h3>Comments:</h3>
+        <div style="margin-bottom:50px;">
+            <textarea class="form-control" rows="3" name="body" placeholder="Leave a comment" v-model="commentBox"></textarea>
+            <button class="btn btn-success" style="margin-top:10px" @click="postComment()">Save Comment</button>
+        </div>
+
+        <div class="media" style="margin-top:20px;" v-for="comment   in comments" >
+            <div class="media-left mx-3">
+                <a href="#">
+                    <img class="media-object" src="http://placeimg.com/80/80" alt="...">
+                </a>
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading border border-danger" v-for="user in comment "> {{user.name}}  </h4>
+                <p>
+                    @{{ comment.body }}
+                </p>
+                <span style="color: #AAA;">on @{{ comment.created_at }}</span>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+		props: [ 'user' , 'post'],
+		data() {
+			return {
+				comments: {},
+				commentBox: '',
+
+			};
+		},
+		mounted() {
+			this.getComments();
+
+		},
+		methods: {
+
+			getComments() {
+				axios.get('/api/posts/' + this.post.id + '/comments')
+					.then((response) => {
+						this.comments = response.data;
+
+					})
+					.catch(function (error) {
+						console.log(error);
+					});
+			},
+			postComment() {
+
+				axios.post('/api/posts/' + this.post.id + '/comments', { body: this.commentBox }, {
+					headers:
+						{
+							'Authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjdmNmQzNTQ1Y2JlYjQ4YjIzODcyMGIxNDM4YTBjYjNkM2JjYTFhYjJjYzVlNjlmZmE2OTMxYTU0OWUxY2IxYzA1ZTFmMWMyNzYxZGU0NzIzIn0.eyJhdWQiOiIxIiwianRpIjoiN2Y2ZDM1NDVjYmViNDhiMjM4NzIwYjE0MzhhMGNiM2QzYmNhMWFiMmNjNWU2OWZmYTY5MzFhNTQ5ZTFjYjFjMDVlMWYxYzI3NjFkZTQ3MjMiLCJpYXQiOjE1NzU5NTQwNjgsIm5iZiI6MTU3NTk1NDA2OCwiZXhwIjoxNjA3NTc2NDY4LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.aZ0M07zHTJZhuz4igUrqK7T6FCnVcljbtlRE__SS1vhmyXUjvimmqxRV27LDujy-YnHsKfdb49DRO4YRZSJtzyI47geMD1k1FiTl0WA4Tw2iEnEsorZyMvyRd9btAiJkoiI6PjFg4iqSuVhVcnmcPXZrdxCBJlouIyxSUJEmt9w_zNxfR0ZMp8U3mdyf49xLBP6p2SZbKUxA5uenW_Wa55yV7pfmbGKJNLKLqn2oT6y6-8Kzcnig1lRpav6glQxnq6Wk8BTXkzXjoaOKkBEFB7YE1J66Kons7VGmKXsgKcJT6B9TnHxOfjIVT1OcHVkZ9SRKErN8jIfZZDe9Z_wZcVog6_NZku-CsXjsquRv2VQkzK6mOcpkzDrmjsY8H11O4UcG73OxunOxjtPeV5XT-L0xu4lfyAXpGAVVu8fGetj3nttxz_2BNT1Ke7wvB0O0L-FTvZhc_c9pn3HHRYvPzlnOqP-FbDscyMXyk4iVA_WVIdKH_KNV7pinSQ5iC-95dUPbTuLXaLPHOrsWpqxFEL0tgIqqR8Y09K4bhaUO52bDK_-Xwvz2Y94kwtdHt9BeBEoqUffhMs5zHevabsIepq7iwPS9tBe5Hl5-Yadrvi8Dk-L1SNDbAECEE9P6YZYRTxA3o1kJNV3CUm7kV5MFoOd8FfvmlsmJTRDal2hG5gs',
+						},
+				})
+					.then((response) => {
+						this.comments.unshift(response.data);
+						this.commentBox = '';
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			},
+		},
+	};
+</script>
